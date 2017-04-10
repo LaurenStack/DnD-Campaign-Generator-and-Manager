973346bfbeae17895ce1b9481b8b5f77292987f3
Dylan Stackhouse and Katie Reed <dylan+katie@>
2017-04-10 14:39:15 -0700

    add color ppick option for map creation and editing

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,13 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-  canvas = null;
-  ctx = null;
-  canvasWidth = 500;
-  canvasHeight = 500;
-  gridSize = 30;
-  grid  = [];
-  renderInterval = null;
+  canvas = null;//: HTMLCanvasElement= null;
+  ctx: CanvasRenderingContext2D = null;
+  canvasWidth:number = 500;
+  canvasHeight:number = 500;
+  gridSize:number = 30;
+  tileHeight: number = this.canvasHeight/this.gridSize;
+  tileWidth: number = this.canvasWidth/this.gridSize;
+  grid: Array<Array<any>> = [];
+  renderInterval:number = null;
+  drawing:boolean = false;
+  color:string = "#000";
 
   constructor() { }
 
@@ -21,16 +25,14 @@ export class MapComponent implements OnInit {
     for(var x = 0; x < size; x ++){
       this.grid.push([]);
       for(var y = 0; y < size; y++){
-        var height = this.canvasHeight/size;
-        var width = this.canvasWidth/size;
 
         this.grid[x].push({
-          color:"rgba(80, 80, 80 , 0.5)",
-          height: height,
-          width: width,
-          x: x*width,
-          y: y*height,
-          stroke:"#fff"
+          color:"rgba(200, 200, 200 , 0.5)",
+          height: this.tileHeight,
+          width: this.tileWidth,
+          x: x*this.tileWidth,
+          y: y*this.tileHeight,
+          stroke:"rgba(100, 100, 100,0.5)"
         });
 
       }//end y loop
@@ -38,7 +40,7 @@ export class MapComponent implements OnInit {
   }//end fill
 
   ngOnInit() {
-    this.canvas = document.getElementById("map");;
+    this.canvas = document.getElementById("map");
     this.ctx = this.canvas.getContext("2d");
 
     this.fill(this.gridSize);
@@ -67,6 +69,33 @@ export class MapComponent implements OnInit {
         }//end y loop
       }//end x loop
     },20);
+  }
+
+  drawTile(eData) {
+    if(this.drawing){
+      var mapCanvas = this.canvas.getBoundingClientRect();
+      var mouseX = eData.clientX - mapCanvas.left;
+      var mouseY = eData.clientY - mapCanvas.top;
+      mouseX = Math.floor(mouseX/this.tileWidth);//changed this.gridSize to this.tileWidth
+      mouseY = Math.floor(mouseY/this.tileHeight);
+      this.grid[mouseX][mouseY].color = this.color;
+    }
+  }
+
+  clearGrid() {
+    location.reload();
+  }
+
+  isDrawing(bool, e){
+    this.drawing = bool;
+    if(bool){
+      this.drawTile(e);
+    }
+  }
+
+  colorChange(e) {
+    this.color = e;
+    console.log(e);
   }
 
 }
